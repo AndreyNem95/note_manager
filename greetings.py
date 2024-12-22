4beb0f0b9c6c005731587f520418821f14064a0a
AndreyNem95 <nemchinovan95@gmail.com>
2024-12-22 13:05:23 +0800

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+from time import process_time_ns
+
+username = input('Введите имя:')
+title = input('Заголовок заметки:')
+content = input('Описание заметки:')
+status =  input('Статус заметки:')
+created_date = input('Дата создания заметки:')
+issue_date = input('Дата истечения заметки')
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_date)
+print(issue_date)
