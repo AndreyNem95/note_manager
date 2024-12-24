aedc5c153885bd641a0b048c7a7a41963455bd7c
AndreyNem95 <nemchinovan95@gmail.com>
2024-12-24 22:30:37 +0800

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,6 @@
+created_date = input('Дата создания заметки (в формате-01.01.2000):')
+issue_date = input('Дата истечения заметки (в формате-01.01.2000):')
+temp_created_date = created_date[0:5]
+temp_issue_date = issue_date[0:5]
+print(temp_created_date)
+print(temp_issue_date)
